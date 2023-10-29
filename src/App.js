e34f9de95ca41a398c0ae43ebde9d6d976535851
lonewolf <thechampthapa@gmail.com>
2023-10-29 21:59:28 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Home from "./components/Home"
+
+function App() {
+  return (
+    <>
+      <div style={{ display: "flex", justifyContent: "center", fontWeight: "bold" , fontSize: "22px", marginTop:"22px"}}>
+        GM_LoneWolf
+      </div>
+      <Home />
+    </>
+  );
+}
+
+export default App;
